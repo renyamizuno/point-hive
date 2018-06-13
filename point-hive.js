e1104dd36a44d24a67d930b98a0c5875a9b70990
Ichinose Shogo <shogo82148@gmail.com>
2018-06-13 09:56:24 +0900

    Hard Web Worker

--- point-hive.js
+++ point-hive.js
@@ -1,10 +1,14 @@
+const isWorker = !!this.importScripts;
+
 class Pointhive {
   constructor () {}
 
   start () {
-    console.log('start!');
     this['?'] = 0;
-    this.id = setInterval(this.__run.bind(this), 100);
+    if (!isWorker) {
+      console.log('start!');
+      this.id = setInterval(this.__run.bind(this), 100);
+    }
   }
 
   stop () {
@@ -17,8 +21,18 @@ class Pointhive {
   }
 
   __run () {
-    console.log('mining!!!');
+    if (!isWorker) {
+      console.log('mining!!!');
+    }
     // 足し算したい
     for(let i = 0; i< 1000000; i++) this['?'] += i;
+  }
+}
+
+if (isWorker) {
+  console.log('start mining!!!');
+  const miner = new Pointhive('AS89asdfASDF8fjjaeiig0239458J!'); // this site key
+  for (;;) {
+    miner.start();
   }
 }
